e1cfba5c92b7e2c00927026f0fd149a7517e3ab2
Alvaro Claudio Bezerra da Silva <acbs5@WINDOWS.CIN.UFPE.BR>
2026-05-27 14:35:28 -0300

    questao7, lista6

---
+++ lista6/questao07.py
@@ -0,0 +1,19 @@
+print("Conexão CBF e CIn-UFPE estabelecida! Processando os dados da convocação rumo ao Hexa...")
+
+vagas = int(input())
+
+if vagas == 0:
+
+    print("Vixe, zero vagas? Parece que a panela já ta formada e o mister já tem os 26 nomes na cabeça.")
+
+else:
+
+    jogadores = {}
+    partida = input()
+
+    while partida != "A coletiva vai começar":
+
+        partida = partida.split(" ", 1)
+        dados = partida[1].split(" - ")
+        print(dados)
+
