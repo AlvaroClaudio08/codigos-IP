79d12ce1333b4b7266bcedd99e3968a0a7e51676
acbs5 <acbs5@g5c11.cin.ufpe.br>
2026-05-28 09:36:06 -0300

    atualizacao L6Q7

--- lista6/questao07.py
+++ lista6/questao07.py
@@ -12,6 +12,8 @@ def partidas():
 
         dados = tuple([int(i) for i in dados])
 
+        if jogador == "Neymar" and dados[]
+
         if jogador not in jogadores:
             jogadores[jogador] = dados
         
@@ -28,13 +30,63 @@ def partidas():
 
     return jogadores
 
-def convocacao():
+def convocacao(jogadores):
 
-    
+    ordenado = {}
+    melhor = ((-1, -1, -1, 9999), "z")
+    #Base: (Gols * 5) + (Assistências * 3) + (Dribles * 1) - (Lesões * 10)
+
+    for i in range(len(jogadores)):
+
+        for jogador in jogadores:
+
+            dados_atual = (jogadores[jogador], jogador)
+
+            if jogador == "Neymar":
+
+                anceloti_score_atual = 20 + dados_atual[0][0]*5 + dados_atual[0][1]*3 + dados_atual[0][2] - dados_atual[0][3]
+
+                if melhor[1] == "Neymar":
+
+                    anceloti_score_melhor = 20 + melhor[0][0]*5 + melhor[0][1]*3 + melhor[0][2] - melhor[0][3]
+
+                else:
+
+                    anceloti_score_melhor = melhor[0][0]*5 + melhor[0][1]*3 + melhor[0][2] - melhor[0][3]*10
+
+            else: 
+
+                anceloti_score_atual = dados_atual[0][0]*5 + dados_atual[0][1]*3 + dados_atual[0][2] - dados_atual[0][3]*10
+
+                if melhor[1] == "Neymar":
+
+                    anceloti_score_melhor = 20 + melhor[0][0]*5 + melhor[0][1]*3 + melhor[0][2] - melhor[0][3]
+
+                else:
+
+                    anceloti_score_melhor = melhor[0][0]*5 + melhor[0][1]*3 + melhor[0][2] - melhor[0][3]*10
+            
+            #criterios:
+            # Maior Ancelotti Score.
+            # Maior número de Gols totais.
+            # Ordem alfabética do nome (A-Z).
+
+            if (anceloti_score_atual > anceloti_score_melhor
+                or dados_atual[0][0] > melhor[0][0]
+                or dados_atual[1] < melhor[1]
+            ):
+                
+                melhor = dados_atual
+
+        ordenado[melhor[1]] = melhor[0]
+        removido = jogadores.pop(melhor[1])
+
+    return ordenado
 
 
 
 print("Conexão CBF e CIn-UFPE estabelecida! Processando os dados da convocação rumo ao Hexa...")
+print()
 
 vagas = int(input())
 
@@ -51,5 +103,5 @@ else:
         print("Ue, a coletiva começou mas ninguém foi analisado? O professor vai convocar os gandulas?")
         jogadores = partidas()
 
-convocacao()
+jogadores_ordenados = convocacao(jogadores)
 
